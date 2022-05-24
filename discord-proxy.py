feb7b83d935896a2c3e78b797cb13573bc4b6dd6
RoootTheFox <rooot@foxes4life.net>
2022-05-24 20:23:10 +0200

    Initial commit working version pog

---
+++ discord-proxy.py
@@ -0,0 +1,86 @@
+from textwrap import indent
+from mitmproxy import http
+from bs4 import BeautifulSoup
+import json 
+import os
+
+def response(flow: http.HTTPFlow):
+        flow.response.headers["content-security-policy"] = "default-src * 'unsafe-inline' 'unsafe-eval'; img-src * blob: data:; media-src * blob: data:"
+        flow.response.headers["access-control-allow-origin"] = "*"
+        if "content-type" in flow.response.headers:
+                print("content type is included in headers!")
+                if flow.response.headers["content-type"] == "text/html" and flow.request.pretty_url.startswith("https://discord.com/app"):
+                        print("Received Discord App Response!")
+                        print("Parsing HTML")
+                        content = flow.response.content
+                        soup = BeautifulSoup(content, 'html.parser')
+                        
+                        print("Loading script")
+                        script_file = open("loader.js")
+                        script = script_file.read()
+                        script_file.close()
+
+                        print("Injecting JS")
+                        tag = soup.new_tag("script")
+                        tag.append(script)
+                        soup.find("head").append(tag)
+
+                        flow.response.text = soup.prettify()
+
+def request(flow: http.HTTPFlow):
+        if flow.request.pretty_url.startswith("https://discord.com/themeldr"):
+                # currently unused, might use this to make installing the loader easier in case injecting js at load is against discord tos
+                if flow.request.pretty_url == "https://discord.com/themeldr/getldr":
+                        loader_file = open("loader.js")
+                        loader = loader_file.read()
+                        loader_file.close()
+                        flow.response = http.Response.make(
+                                200,
+                                loader,
+                                {"Content-Type": "text/javascript"}
+                                )
+                if flow.request.pretty_url == "https://discord.com/themeldr/modifytheme":
+                        data = flow.request.content.decode('utf-8')
+                        data_json = json.loads(data)
+                        print("Theme: " + data_json["name"], "Enabled: " + str(data_json["enabled"]))
+                        
+                        datastore = {}
+                        if os.path.exists("datastore.json"):
+                                datastore_file = open("datastore.json", "r")
+                                datastore = json.load(datastore_file)
+                        else:
+                                print ("no datastore.json")
+                                datastore = {}
+                        
+                        print(datastore)
+                        if "themes" not in datastore:
+                                print("no themes in datastore")
+                                datastore["themes"] = []
+                        
+                        already_indexed = False
+                        for i in range(len(datastore["themes"])):
+                                if datastore["themes"][i]["name"] == data_json["name"]:
+                                        already_indexed = True
+                                        datastore["themes"][i]["enabled"] = data_json["enabled"]
+                                        datastore["themes"][i]["url"] = data_json["url"]
+                                        datastore["themes"][i]["data"] = data_json["data"]
+                        
+                        if not already_indexed:
+                                datastore["themes"].append({"name": data_json["name"], "enabled": data_json["enabled"], "url": data_json["url"], "data": data_json["data"]})
+                        
+                        with open('datastore.json', 'w') as outfile:  
+                                json.dump(datastore, outfile, indent=4)
+                        
+                        flow.response = http.Response.make(
+                                200,
+                                "",
+                                {"Content-Type": "text/plain"}
+                                )
+                
+                if flow.request.pretty_url == "https://discord.com/themeldr/getthemes":
+                        if os.path.exists("datastore.json"):
+                                datastore_file = open("datastore.json", "r")
+                                datastore = json.load(datastore_file)
+                                flow.response = http.Response.make(200, json.dumps(datastore["themes"], indent=4), {"Content-Type": "application/json"})
+                        else:
+                                flow.response = http.Response.make(200,"[]",{"Content-Type": "application/json"})
---
+++ loader.js
@@ -0,0 +1,172 @@
+// functions
+console.log("THEME LOADER");
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function _getElement(e, baseElement = document) {
+    if (e instanceof Node) return e;
+    return baseElement.querySelector(e);
+}
+
+function _escapeID(id) {
+    return id.replace(/^[^a-z]+|[^\w-]+/gi, "-");
+}
+
+function _injectTheme(id, css) {
+    id = "theme-" + _escapeID(id);
+    const style = _getElement(`#${id}`) || document.createElement("style");
+    console.log("id: " + id);
+    style.id = id;
+    style.textContent = css;
+    document.head.append(style);
+}
+
+function loadTheme(url) {
+    console.log("Loading theme from url: " + url);
+
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            console.log("* Loaded theme, length " + data.length);
+
+            const data_string = data.toString();
+
+            var themeName = "unknown-" + btoa((Math.random(2147483647)).toString(36).substring(2)).substring(0, 8);
+
+            if (data_string.includes("@name ")) {
+                var tmp = data_string.substring(data_string.indexOf("@name"));
+                tmp = tmp.substring(6, tmp.indexOf("\n"));
+                themeName = tmp;
+            }
+
+            themeName = _escapeID(themeName);
+
+            // TODO: check what betterdiscord specific things exist
+            addTheme(themeName, data_string, url);
+        });
+}
+
+async function getThemes() {
+    var response = await fetch("https://discord.com/themeldr/getthemes");
+    var res_json = await response.json();
+    return res_json;
+}
+
+function setThemes(themes) {
+    fetch('https://httpbin.org/post', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                a: 7,
+                str: 'Some string: &=&'
+            })
+        }).then(res => res.json())
+        .then(res => console.log(res));
+    return setCookie("discordthemeldr-themes", themes);
+}
+
+async function getEnabledThemes() {
+    var themes = await getThemes();
+    var enabled = [];
+    for (var i = 0; i < themes.length; i++) {
+        if (themes[i].enabled) enabled.push(themes[i]);
+    }
+    return enabled;
+}
+
+async function addTheme(name, css, url) {
+    var theme = css;
+    if ( /*data.toString().contains("bd-")*/ true) {
+        console.log("* Patching theme");
+
+        // todo - add theme patching code here (if needed)
+
+        console.log("* Injecting theme");
+
+        _injectTheme(name, theme);
+    } else {
+        console.log("* Injecting theme");
+        let theme = document.createElement("link");
+        theme.rel = "stylesheet";
+        theme.href = url;
+        document.head.append(theme);
+    }
+
+    console.log("adding to cookie");
+    var themes = await getThemes();
+    var found = false;
+    for (var i = 0; i < themes.length; i++) {
+        if (themes[i].name == name) {
+            console.log("found")
+            found = true;
+            themes[i].enabled = true;
+            themes[i].url = url;
+        }
+    }
+
+    if (!found) {
+        console.log("not found, adding to themes array");
+        themes.push({
+            name: name,
+            enabled: true,
+            url: url
+        });
+    }
+
+    fetch('https://discord.com/themeldr/modifytheme', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                enabled: true,
+                url: url,
+                data: theme
+            })
+        }).then(res => res.text())
+        .then(res => console.log(res));
+
+    setThemes(JSON.stringify(themes));
+}
+
+console.log("Functions loaded. Use loadTheme(\"url\") to load a theme and clearTheme() or CTRL-R to unload.");
+
+console.log("Getting saved themes");
+
+async function themeldr_init() {
+    var themes = await getThemes();
+    var themes_enabled = await getEnabledThemes();
+    
+    console.log("Themes: " + JSON.stringify(themes));
+    console.log("Enabled: " + JSON.stringify(themes_enabled));
+    
+    for (var i = 0; i < themes_enabled.length; i++) {
+        if (themes_enabled[i].enabled) {
+            loadTheme(themes_enabled[i].url);
+        }
+    }
+}
+
+setTimeout(themeldr_init(), 10);
